68c1d1aa6fceb363d6176fa6ce0721347019a555
noeliapasaca <napasaca@espol.edu.ec>
2024-08-20 22:22:03 -0500

    Updates

--- app.js
+++ app.js
@@ -1,14 +1,23 @@
- /* 1. Carga de variables de entorno */
- require('dotenv').config()
+require('dotenv').config()
 
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
+
+/* 1. Módulo express-session */
+const session = require('express-session');
+
+/* 1. Referencia a los middlewares */
+var authenticateSession = require('./middleware/authentication_session');
+var authorizationSession = require('./middleware/authorization_session');
+var authorizationToken = require('./middleware/authorization_token');
+
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var tokenRouter = require('./routes/token');
 
 var app = express();
 
@@ -17,13 +26,25 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
+
+/* 2. Configuración del middleware */
+app.use(session({
+  secret: process.env.TOKEN_SECRET,
+  name: 'session.security', 
+  resave: false,
+  saveUninitialized: false,
+}));
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+/* 2. Agregue el middleware al router */
+app.use('/users', authenticateSession, authorizationSession, usersRouter);
+app.use('/token', authenticateSession, authorizationToken, tokenRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
